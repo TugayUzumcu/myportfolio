fd1b2c5e282f71dc13305d570ad7c49293830506
TugayUzumcu <tuuzinli@gmail.com>
2016-12-27 20:45:52 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,11 @@
+document.getElementById("tekst").value = localStorage.getItem("tekst");
+
+function initStorage(){
+function saveName()
+	{
+		var name = document.getElementById('tekst'); 
+		localStorage.setItem('tekst',tekst.value);
+	}
+	saveName();
+				} 
+				 
