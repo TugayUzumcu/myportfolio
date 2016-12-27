516e5fc36d08e28b353d21aac5eb250bee871859
TugayUzumcu <tuuzinli@gmail.com>
2016-12-27 21:33:42 +0100

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,11 +1,36 @@
-document.getElementById("tekst").value = localStorage.getItem("tekst");
+window.onload=greet();		 
 
-function initStorage(){
-function saveName()
-	{
-		var name = document.getElementById('tekst'); 
-		localStorage.setItem('tekst',tekst.value);
-	}
-	saveName();
-				} 
-				 
+function greet(){
+    name = localStorage.getItem("name");
+    if (name == null || name == "null"){
+      alert("Hi, Stranger!");
+      name = prompt("What is your name?");
+      localStorage.setItem("name", name);
+    } else {
+      
+		alert ("Hi, " + name + "!");
+    } // end greet
+  } // end function 
+
+ var myString = ("Welkom, " + localStorage.getItem("name"));
+			 var myArray = myString.split("");
+			 var loopTimer;
+
+			 function frameLooper()
+			 {
+			 	if (myArray.length > 0) 
+			 	{
+			 		document.getElementById("myTypingText").innerHTML += myArray.shift();
+			 		console.log(myArray);
+			 		loopTimer = setTimeout('frameLooper()',80);
+
+			 	}
+			 	else
+			 	{
+			 		clearTimeout(loopTimer);
+			 		console.log("stop!");
+			 	}
+			 	
+
+			 }
+			frameLooper();
